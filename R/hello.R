6ccac09a0684729f37283cbb48b0350e97b965d7
Bruno Gómez <br1_gio@hotmail.com>
2021-05-21 19:56:46 -0500

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -38,7 +38,6 @@ piv_preliminar <- function(i_ciclo,i_programa,i_reporte,i_analizador) {
   #identificar analitos
   id_analitos<-c("A1","A2","A3","A4","A5","A6","A7","A8","A9","A10")
   df <- data.frame(id_reporte,piv,id_analitos,iv_analitos)
-  #regresar JSON
 
   return(df)
 }
