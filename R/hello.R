b4177b5fb5c2c3aa4c8ec7fa7ab6ed52b6c3bd4d
Bruno Gómez <br1_gio@hotmail.com>
2021-05-23 17:52:14 -0500

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -71,3 +71,30 @@ plot_test <- function() {
   print(g1)
 }
 
+gitstats <- function (id = "hadley", type = c("users", "orgs"), max = 20) {
+
+  type <- match.arg(type, choices=c('users','orgs'))
+  max <- min(max, 100)
+
+  #the 'gh' package automatically paginates
+  url <- file.path("https://api.github.com", type, id, "repos")
+  res <- gh::gh(url, type = "owner", .limit = Inf)
+  out <- jsonlite:::simplify(res, flatten = TRUE)
+
+  #resort factor)
+  out <- out[order(out$watchers, decreasing = TRUE)[seq_len(max)],
+             c("name", "watchers", "forks", "open_issues")]
+  out$name <- factor(out$name, levels = rev(out$name))
+
+  #reshape to "long" dataframe"
+  names(out) <- c("Repo", "Stars", "Forks", "Issues")
+  out2 <- reshape2::melt(out, id = 1)
+
+  #create ggplot object
+  time <- format(Sys.time(), tz = 'UTC', usetz = TRUE)
+  ggplot(out2, aes(Repo, value)) + geom_bar(stat="identity") + coord_flip() +
+    facet_wrap(~variable, scales = "free_x") + xlab("") + ylab("") +
+    ggtitle(sprintf("Github stats from: '%s' (%s)", id, time))
+
+}
+
